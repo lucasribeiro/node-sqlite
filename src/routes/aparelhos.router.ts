1a48c18ef16a691c1053746404e34e2016c1c2da
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2023-06-21 20:03:26 -0300

    Commit Incial

--- src/routes/aparelhos.router.ts
+++ src/routes/aparelhos.router.ts
@@ -3,14 +3,6 @@ const knex = require('knex')(require('../../knexfile.js').development);
 
 const aparelhosRouter = Router();
 
-const aparelhos = [
-        { id: 1, descricao: "Galaxy S22", valor: 4500.00, marca: "Samsung"  },
-        { id: 2, descricao: "iPhone 14", valor: 7000.00, marca: "Apple"  },
-        { id: 3, descricao: "Moto G9", valor: 2500.00, marca: "Motorola"  },
-        { id: 4, descricao: "Egde 5G", valor: 5000.00, marca: "Motorola"  },
-        { id: 5, descricao: "Xperia Z9", valor: 8000.00, marca: "Sony"  },
-    ]
-
 // Cria um manipulador da rota padrão
 aparelhosRouter.get('/:id', async (request, response) => {
    await knex('aparelhos').where('id', request.params.id)
